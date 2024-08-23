e2903150f959be7003e30a2f75498cde5811682b
Jacob Angeles <angeles.jacob@gmail.com>
2024-08-23 10:52:15 +0200

    modified implemention: class Server
    Co-Authored-By: andreacrox <178835498+andreacrox@users.noreply.github.com>
    Co-Authored-By: Francesca <166231293+francescadoriano@users.noreply.github.com>

--- server/Server.java
+++ server/Server.java
@@ -13,19 +13,46 @@ public class Server {
         Scanner userInput = new Scanner(System.in);
 
         try {
+            // risorsa condivisa tra tutti i thread
+            TopicManager topicManager = new TopicManager(); 
+
             ServerSocket server = new ServerSocket(port);
             /*
              * deleghiamo a un altro thread la gestione di tutte le connessioni; nel thread
              * principale ascoltiamo solo l'input da tastiera dell'utente (in caso voglia
              * chiudere il programma)
              */
-            Thread serverThread = new Thread(new SocketListener(server));
+            Thread serverThread = new Thread(new SocketListener(server, topicManager));
             serverThread.start();
 
             String command = "";
 
             while (!command.equals("quit")) {
                 command = userInput.nextLine();
+
+                // nel caso del commando "show", delega la gestione di IO ad un thread separato estrarre tutti i topic
+                if (command.startsWith("show")) {
+                    Thread topicExtractor = new Thread(new TopicExtractor());
+                    topicExtractor.start();
+                    try {
+                        topicExtractor.join();
+                    } catch (InterruptedException e) {
+                        //se qualcuno interrompe questo thread nel frattempo, terminiamo
+                        return;
+                    }
+                // nel caso del commando "inspect", delega la gestione di IO ad un thread separato per il Subscriber
+                } else if (command.startsWith("inspect")) {
+                    Thread topicInspector = new Thread(new TopicInspector());
+                    topicInspector.start();
+                    try {
+                        topicInspector.join();
+                    } catch (InterruptedException e) {
+                        //se qualcuno interrompe questo thread nel frattempo, terminiamo
+                        return;
+                    }
+                } else {
+                    System.out.println("Unknown command");
+                }
             }
 
             try {
