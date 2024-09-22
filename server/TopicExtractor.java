3f1232a84a91e87002e10947f607b5bcaab92baf
Francesca <francescadoriano22@gmail.com>
2024-09-22 21:13:41 +0200

    implementato la classe TopicExtractor

--- server/TopicExtractor.java
+++ server/TopicExtractor.java
@@ -1,10 +1,32 @@
+import java.util.List;
 
 public class TopicExtractor implements Runnable {
+    //attraverso questo oggetto possiamo accedere ai topic memorizzati
+    private final TopicManager topicManager;
+
+    public TopicExtractor(TopicManager topicManager) {
+        this.topicManager = topicManager
+    }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
-    }
+        try {
+            //estrazione dei topic
+            List<String> topics = topicManager.getTopics();
 
-}
+            //mostra topics
+            if (topics.isEmpty()) {
+                System.out.println("Nessun topic disponibile.");
+            } else {
+                System.out.println("Topics:");
+                for (String topic : topics) {
+                    System.out.println("- " + topic);
+                }
+            }
+            } catch (Exception e) {
+                System.out.println("Si e' verificato un errore durante l'estrazione dei topic: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+    
