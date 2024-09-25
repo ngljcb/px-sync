bb29462a8f5b4b564bf3a18dbf47cf5d11349e83
Jacob Angeles <angeles.jacob@gmail.com>
2024-09-25 21:50:29 +0200

    minor fix

--- server/TopicExtractor.java
+++ server/TopicExtractor.java
@@ -1,32 +1,9 @@
-import java.util.List;
-
 public class TopicExtractor implements Runnable {
-   //attraverso questo oggetto possiamo accedere ai topic memorizzati
-   private final TopicManager topicManager;
 
-   public TopicExtractor(TopicManager topicManager) {
-       this.topicManager = topicManager;
-   }
-   
-    @Override   
+    @Override
     public void run() {
-        try {
-            //estrazione dei topic
-            List<String> topics = topicManager.getTopics();
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
 
-            //mostra topics
-            if (topics.isEmpty()) {
-                System.out.println("Nessun topic disponibile.");
-            } else {
-                System.out.println("Topics:");
-                for (String topic : topics) {
-                    System.out.println("- " + topic);
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Si e' verificato un errore durante l'estrazione dei topic: " + e.getMessage());
-            e.printStackTrace();
-        }
-    } 
-
-}   
+}
